bda08a4dcab3836bb80905341ed5786348809e99
luka1chkhaidze <lukachkhaidze087@gmail.com>
2024-03-16 17:08:53 +0400

    commit

---
+++ day 0/day1.py
@@ -0,0 +1,7 @@
+#first day (Execute Python Sintax)
+
+#example 1:
+print("hello world")
+#if we run this code with the top right "triangle" it will give us the word inside the code.
+#________python indentation________
+
