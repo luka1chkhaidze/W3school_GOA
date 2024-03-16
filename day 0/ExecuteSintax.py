3a4d10176d1a83c248847b26f3fc5477b0279167
Luka1chkhaidze <lukachkhaidze087@gmail.com>
2024-03-16 17:52:39 +0400

    commit

---
+++ day 0/ExecuteSintax.py
@@ -0,0 +1,39 @@
+#first day (Execute Python Sintax)
+
+#example 1:
+print("hello world")
+#if we run this code with the top right "triangle" it will give us the word inside the code.
+#________python indentation________
+
+#Indentation refers to the spaces at the beginning of a code line.
+#Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+#Python uses indentation to indicate a block of code.
+
+
+#example 2:
+if 5 > 4:
+  print("five is greater than four")
+#IT WONT WORK WITHOUT THE DOUBLE PERIOD !
+  
+#Example 3:
+
+if 5 > 2:
+ print("five is greater than two")
+
+#its relly up to us how much distance do we need between these 2 command but IT HAS TO BE ATLEAST 1 SPACE.
+ 
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
+
+
+#You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+
+if 5 > 2:
+ print("Five is greater than two!")
+    print("Five is greater than two!")
+
+
+
+
--- day 0/day1.py
+++
@@ -1,7 +0,0 @@
-#first day (Execute Python Sintax)
-
-#example 1:
-print("hello world")
-#if we run this code with the top right "triangle" it will give us the word inside the code.
-#________python indentation________
-
