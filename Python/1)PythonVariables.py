51b2e28d2e9add5018559e9fb990b3cc58347b1d
Luka1chkhaidze <lukachkhaidze087@gmail.com>
2024-03-22 15:52:08 +0400

    commit

---
+++ Python/1)PythonVariables.py
@@ -0,0 +1,41 @@
+#lets say we got a number and we whant to give a variable to this value(number)
+#Example 1:
+
+x = 4
+y = 9
+print(x,y)
+
+#x and y in this case are varibles, 4 and 9 are values for this variable
+
+#Python has no command for declaring a variable.
+
+#we also have 3 different DATA TYPES
+
+q = 23 #we call this a integer-int
+w = "house" #we call this a string-str
+e = 0.9 #we call this float
+
+"""
+if we whant do not know what data type we got
+to handle with, Pyton got us coverd ;)
+"""
+x = 23
+y = "house"
+a = 0.9
+
+#we need to know the data type for connectin these Variables to gether
+
+print(type(x))
+print(type(y))
+print(type(a))
+#this will show us the data type of theese Variables in the terminal by running the code.
+#_________________________________________________________________________________________
+# lets do something like this
+x = 3
+X = 5
+print(x,X)
+
+#if there would of been 2 undercase x-is the therinal will put out the last variables value (in this case 3).
+x = 2
+x = 3
+print(x,x)
---
+++ Python/3)comments.py
@@ -0,0 +1,15 @@
+#this is a comment, it is for us who needs a hint for explination of any codeblock.
+#comment automatically gets ignored by python and youre computer, thanks the name of COMMENT.
+
+#Example 1:
+
+print("GOA is the best academy in the whole world") #this is the truest thing in the whole world
+                                                    #^^^^^^^^^^^^^^^^^^^this is a comment^^^^^^^
+
+#we can also use multiline comment
+#Example 2:
+
+"""
+GOA truly is the best academy in the world
+GOA is the freandliest academy
+"""
